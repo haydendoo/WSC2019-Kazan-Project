49990877f572ae7bf497d7c2f18207d1d8945c8c
haydendoo <haydenhow@gmail.com>
2026-04-24 01:14:55 +0800

    fixes

--- infrastructure/init.sql
+++ infrastructure/init.sql
@@ -1,8 +1,12 @@
-CREATE DATABASE IF NOT EXISTS database;
-USE database;
+CREATE DATABASE IF NOT EXISTS unicorndb;
+USE unicorndb;
 
 CREATE TABLE IF NOT EXISTS unicorns (
     id INT AUTO_INCREMENT PRIMARY KEY,
     redis_token varchar(255) NOT NULL,
     filesystem_token varchar(255) NOT NULL
 );
+
+CREATE USER IF NOT EXISTS 'username'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON unicorndb.* to 'username'@'%';
+FLUSH PRIVILEGES;
